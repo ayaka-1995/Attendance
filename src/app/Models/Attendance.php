4def4ef2be5a41b022cd90a93f9ec8aed54b82b1
isii ayaka <ayaka950210@gmail.com>
2025-09-25 22:56:35 +0900

    勤怠一覧修正

--- src/app/Models/Attendance.php
+++ src/app/Models/Attendance.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Attendance extends Model
 {
@@ -22,5 +23,23 @@ class Attendance extends Model
     public function breaks()
     {
         return $this->hasMany(BreakModel::class);
+    }
+
+    public function getTotalBreakTimeAttribute()
+    {
+        $totalMinutes = 0;
+
+        foreach ($this->breaks as $break) {
+            if ($break->break_start_time && $break->break_end_time) {
+                $start = Carbon::parse($break->break_start_time);
+                $end = Carbon::parse($break->break_end_time);
+                $totalMinutes += $end->diffInMinutes($start);
+            }
+        }
+
+        $hours = floor($totalMinutes / 60);
+        $minute = $totalMinutes % 60;
+
+        return sprintf('%02d:%02d', $hours, $minute);
     }
 }
